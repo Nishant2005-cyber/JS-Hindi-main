c05f201ee1dbee32d8e0a9bb00d21c05f091603d
Nishant2005-cyber <nishukhandelwal012@gmail.com>
2025-08-03 10:41:09 +0530

    Arrows

---
+++ 03_basics/03_Arrow.js
@@ -0,0 +1,46 @@
+const user = {
+    username: "Nishant",
+    price: 999,
+
+    welcomeMessage: function() {
+        console.log(`${this.username} , welcome to website`);
+        console.log(this);
+    }
+}
+
+// user.welcomeMessage()
+// user.username = "Sam"
+// user.welcomeMessage()
+
+// console.log(this);
+
+// function chai(){
+//     let username = "Nishant"
+//     console.log(this.username);
+    
+// }
+
+// const chai = function () {
+//     let username = "Nishant"
+//     console.log(this.username);
+// }
+
+// const chai = () => {
+//     let username = "Nishant"
+//     console.log(this.username);
+// }
+
+// chai()
+
+// const addTwo = (num1, num2) => {
+//     return num1 + num2
+// }
+
+// const addTwo = (num1, num2) => num1 + num2
+
+// const addTwo = (num1, num2) => (num1 + num2)
+
+const addTwo = (num1, num2) => ({username: "Nishant"})
+
+console.log(addTwo(3, 4));
+
