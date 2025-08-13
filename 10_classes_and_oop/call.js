b8dc5c7aa82bf1283acf9177f35fba26d9f66a4b
Nishant2005-cyber <nishukhandelwal012@gmail.com>
2025-08-13 23:59:30 +0530

    prototype and call

---
+++ 10_classes_and_oop/call.js
@@ -0,0 +1,15 @@
+function setUsername(username) {
+    this.username = username;
+    console.log("called");
+    
+}
+
+function createUser(username, email, password) {
+    setUsername.call(this, username)
+
+    this.email = email
+    this.password = password
+}
+
+const User = new createUser("Nishant", "Nishu@fb.com", "123")
+console.log(User);
